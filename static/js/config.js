afb96657d95a77458628c90734375c4bd234123e
love666you <93461125+love666you@users.noreply.github.com>
2023-05-26 17:53:27 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -2,6 +2,6 @@ const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
    // url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
     url: "https://service-bek22ol0-1318112844.usw.apigw.tencentcs.com/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: ""  // openAi Base64 编码 apiKey
-    //apiKey: "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"  // openAi Base64 编码 apiKey
+    //apiKey: ""  // openAi Base64 编码 apiKey
+    apiKey: "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"  // openAi Base64 编码 apiKey
 };  
