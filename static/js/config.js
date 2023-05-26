e45a19137659c5b7d04a7ce08add7de010c1271f
love666you <93461125+love666you@users.noreply.github.com>
2023-05-26 16:55:55 +0800

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -2,5 +2,6 @@ const config = {
     // gpt3.5 官方接口：https://api.openai.com/v1/chat/completions
    // url: "https://open.aiproxy.xyz/v1/chat/completions",  // gpt3.5代理接口
     url: "https://service-bek22ol0-1318112844.usw.apigw.tencentcs.com/v1/chat/completions",  // gpt3.5代理接口
-    apiKey: ""  // openAi Base64 编码 apiKey
+  //  apiKey: ""  // openAi Base64 编码 apiKey
+    apiKey: "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"  // openAi Base64 编码 apiKey
 };  
