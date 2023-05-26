093dd4bc48b5538cda82c0f4f5b3156b42c3b5ae
love666you <93461125+love666you@users.noreply.github.com>
2023-05-26 17:23:45 +0800

    Update custom.js

--- static/js/custom.js
+++ static/js/custom.js
@@ -247,9 +247,7 @@ $(document).ready(function() {
   if (apiKey) {
      $(".settings-common .api-key").val(apiKey);
   }
-  //else{
-  //  $(".settings-common .api-key").val(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);
- // }
+ 
 
   // apiKey输入框事件
   $(".settings-common .api-key").blur(function() { 
